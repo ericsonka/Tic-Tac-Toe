c77d3ba27263c7f6a2fb3cc7f847debde8a817c1
ericson <ericson8099@gmail.com>
2025-08-17 11:31:44 +0530

    small update

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html' // important for SPA-style routing
+			fallback: 'index.html'  // 👈 important for SPAs on GitHub Pages
 		}),
 		paths: {
-			// If your repo is github.com/username/repo
-			base: dev ? '' : '/Tic-Tac-Toe'
+			base: process.env.NODE_ENV === 'production' ? '/Tic-Tac-Toe' : ''
 		}
 	}
 };
